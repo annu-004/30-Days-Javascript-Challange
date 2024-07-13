3b17e5341332ca769c9c9ef88dda3be22318c5fa
annu-004 <annu11326@gmail.com>
2024-07-13 16:53:35 +0530

    Create 01_day.js
    Day 1 Completed ...<3

---
+++ 30days/01_day.js
@@ -0,0 +1,103 @@
+// Day 1 : Variable and Data Types
+
+// Task/Activities:
+// Activity 1 : Variable Declaration
+// Task 1: 
+
+    var num1 = 100;
+    console.log(num1);
+
+// Task 2: 
+
+     let nick_name = "Adesh";
+     console.log(nick_name);  
+     
+// Activity 2 : Constant Declaration
+// Task 3 :
+
+    const isTaskCompleted = true;
+    console.log(isTaskCompleted);
+
+// Activity 3 : Data Types
+// Task 4 :
+
+    let num2 = 101;
+    console.log(num2);
+
+    let name = "Akash";
+    console.log(name);
+
+    let isUnderstood = true;
+    console.log(isUnderstood);
+
+    let student = {
+        student_name : "Aman",
+        age : 20
+    };
+    console.log(student);
+
+    let makeArray = [1,2,3,4];
+    console.log(makeArray);
+
+    // "console.log([num, name, isUnderstood, student, makeArray]);"
+    // this is also a way to print
+
+// Activity 4 : Reassigning Variables
+//  Task 5 :
+
+    let email = "abc@gmail.com";
+    console.log(email);
+
+    email = "xyz@gmail.com";
+    console.log(email);
+
+// Activity 5 : Understanding Const 
+//  Task 6 :
+
+    const num3 = 7;
+    console.log(num3);
+
+    num4 = 11;
+    console.log(num4);
+    
+    // TypeError : Assignment to constant variable.
+
+
+// Feature Request
+// 1. Variable Types Console Log :
+
+    let myName = "Adi";
+    let myage = 21;
+    let isStudent = true;
+    let challenge = { "30DaysOfJS" : "with Hitesh Sir"};
+    let week = [1,2,3,4];
+
+    console.log([myName, typeof myName , myage , typeof myage , isStudent , typeof isStudent , challenge , typeof challenge , week , typeof week ]);
+
+
+//  2. Reassignment Demo :
+//  let
+
+    let favFood = 'Indian';
+    console.log(favFood);
+
+    favFood = 'Italian';
+    console.log(favFood);
+
+// const
+
+   const favColour = 'Black';
+   console.log(favColour);
+
+   favColour = 'Pink';
+   console.log(favColour);
+
+// showing type error
+
+
+
+
+// Day 1 Completed ...<3
+
+
+   
