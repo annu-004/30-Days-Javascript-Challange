ea429ccd51f78dafeefe09b5128fd0080ed709b9
annu-004 <annu11326@gmail.com>
2024-07-14 17:09:47 +0530

    Updated 02_day.js
    Day 2 Completed...<3

---
+++ 30days/02_day.js
@@ -0,0 +1,63 @@
+// Day 2 : Operators
+
+// Task/Activities
+// Activity 1 : Airthmetic Operations
+// Task 1 / 2 / 3 / 4 / 5 :
+
+   let num1 = 100;
+   let num2 = 20;
+   console.log(`${num1 + num2}`);
+   console.log(`${num1 - num2}`);
+   console.log(`${num1 * num2}`);
+   console.log(`${num1 / num2}`);
+   console.log(`${num1 % num2}`);
+
+
+// Activity 2 : Assignment Operators 
+// Task 6 / 7 :
+
+   console.log(`Old value of num1 : ${num1} and num2 : ${num2}`);
+   num1 += 1;
+   num2 -= 1;
+   console.log(`New Value of num1 : ${num1} and num2 : ${num2}`);
+
+// Activity 3 : Comparison Operators
+// Task 8 / 9 / 10 :
+
+   const age = 20;
+   console.log(`Age is greater than 18? : ${age > 18}`);
+   console.log(`Age is less than 18? : ${age < 18}`);
+   console.log(`Age is greater than or equal to 21? : ${ age >= 21}`);
+   console.log(`Age is less than or equal to 21? ${age <= 21}`);
+
+   const realNum = 7;
+   const stringNum = "7";
+   console.log(`7 == "7"? : ${realNum == stringNum}`);
+   console.log(`7 === "7"? ${realNum === stringNum}`);
+
+// Activity 4 : Logical Operator 
+//  Task 11 / 12 / 13 :
+
+    console.log(`(1 < 2) && (3 < 4)? : ${(1 < 2) && (3 < 4)}`);
+    console.log(`(1 < 2) || (3 < 4)? : ${(1 < 2) || (3 < 4)}`);
+
+    const isLoggedOn = true;
+    console.log(`! of isLoggedOn : ${!isLoggedOn}`);
+
+// Activity 5 : Ternary Operator
+// Task 14 :
+
+   let newNumber = 20;
+   (newNumber >= 0)? console.log("Number is Positive") : console.log("Number is Negative");
+
+
+//   Day 2 Completed ...<3
+
+    
+
+    
+   
+   
+
+
+
